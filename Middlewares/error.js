f6866871dc874ffd54b8af19d19511ba5fc0a890
Manoj-Sh-AI <shmanoj100@gmail.com>
2024-04-08 13:46:28 +0530

    5 th

--- Middlewares/error.js
+++ Middlewares/error.js
@@ -15,6 +15,7 @@ export const errorMiddleware = (err, req, res, next)=> {
     return res.status(err.statusCode).json({
         success: false,
         message: err.message,
+       stack : err.stack
     });
 }
 
